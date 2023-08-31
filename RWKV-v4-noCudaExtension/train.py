5d4c849c6fa21c458930d9c6f4429d11c7ac9901
Ratza <razvandumm@gmail.com>
2023-08-30 19:42:08 -0700

    Even more layers.

--- RWKV-v4-noCudaExtension/train.py
+++ RWKV-v4-noCudaExtension/train.py
@@ -81,7 +81,7 @@ os.environ['USE_WANDB'] = '0' # wandb logging. 0 = False, 1 = True
 EPOCH_BEGIN = 0 # begins with miniEpoch = EPOCH_BEGIN
 LOAD_MODEL = False # shall we load the #EPOCH_BEGIN model and continue the training from it?
 
-n_layer = 12
+n_layer = 16
 n_embd = 512
 ctx_len = 1024 # increase T_MAX in src/model.py if your ctx_len is longer
 
