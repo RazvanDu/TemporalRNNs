cd2555aebf5f51216f188591ece8049803f54d22
Ratza <razvandumm@gmail.com>
2023-09-11 20:27:19 -0700

    Experiments.

--- RWKV-v4-noCudaExtension/cuda/wkv_op.cpp
+++ RWKV-v4-noCudaExtension/cuda/wkv_op.cpp
@@ -1,6 +1,6 @@
 #include <torch/extension.h>
 
-void cuda_forward(int B, int T, int C, float *w, float *u, float *k, float *v, float *y);
+void cuda_forward(int B, int T, int C, float *w, float *u, torch::PackedTensorAccessor32<F,1> k, float *v, float *y);
 void cuda_backward(int B, int T, int C, float *w, float *u, float *k, float *v, float *gy, float *gw, float *gu, float *gk, float *gv);
 
 void forward(int64_t B, int64_t T, int64_t C, torch::Tensor &w, torch::Tensor &u, torch::Tensor k, torch::Tensor &v, torch::Tensor &y) {
