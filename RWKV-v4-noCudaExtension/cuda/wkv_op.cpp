9f09f91cf6a83a527587f339f9708158aaaefbb9
Ratza <razvandumm@gmail.com>
2023-09-11 22:29:00 -0700

    Experiments.

--- RWKV-v4-noCudaExtension/cuda/wkv_op.cpp
+++ RWKV-v4-noCudaExtension/cuda/wkv_op.cpp
@@ -1,10 +1,10 @@
 #include <torch/extension.h>
 
-void cuda_forward(int B, int T, int C, float *w, float *u, torch::PackedTensorAccessor32<float,1, at::DefaultPtrTraits, int> k, float *v, float *y);
+void cuda_forward(int B, int T, int C, float *w, float *u, torch::PackedTensorAccessor32<float,1> k, float *v, float *y);
 void cuda_backward(int B, int T, int C, float *w, float *u, float *k, float *v, float *gy, float *gw, float *gu, float *gk, float *gv);
 
 void forward(int64_t B, int64_t T, int64_t C, torch::Tensor &w, torch::Tensor &u, torch::Tensor k, torch::Tensor &v, torch::Tensor &y) {
-    cuda_forward(B, T, C, w.data_ptr<float>(), u.data_ptr<float>(), k.accessor<float, 1>(), v.data_ptr<float>(), y.data_ptr<float>());
+    cuda_forward(B, T, C, w.data_ptr<float>(), u.data_ptr<float>(), k.packed_accessor32<float, 1>(), v.data_ptr<float>(), y.data_ptr<float>());
 }
 void backward(int64_t B, int64_t T, int64_t C, torch::Tensor &w, torch::Tensor &u, torch::Tensor &k, torch::Tensor &v, torch::Tensor &gy, torch::Tensor &gw, torch::Tensor &gu, torch::Tensor &gk, torch::Tensor &gv) {
     cuda_backward(B, T, C, w.data_ptr<float>(), u.data_ptr<float>(), k.data_ptr<float>(), v.data_ptr<float>(), gy.data_ptr<float>(), gw.data_ptr<float>(), gu.data_ptr<float>(), gk.data_ptr<float>(), gv.data_ptr<float>());
